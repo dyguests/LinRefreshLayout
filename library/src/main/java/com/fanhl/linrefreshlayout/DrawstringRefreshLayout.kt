ffd34a612e4587e28eff5e65d245b465ed91f757
fanhl <fanhonglin@klicen.com>
2018-12-03 17:49:35 +0800

    target

--- library/src/main/java/com/fanhl/linrefreshlayout/DrawstringRefreshLayout.kt
+++ library/src/main/java/com/fanhl/linrefreshlayout/DrawstringRefreshLayout.kt
@@ -2,6 +2,7 @@ package com.fanhl.linrefreshlayout
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.NonNull
 import androidx.annotation.Nullable
@@ -12,18 +13,47 @@ import androidx.annotation.Nullable
 class DrawstringRefreshLayout @JvmOverloads constructor(
     @NonNull context: Context, @Nullable attrs: AttributeSet? = null
 ) : ViewGroup(context, attrs) {
+    /** the target of the gesture */
+    private var mTarget: View? = null
+
+    /** 下拉view */
     private val drawstringView by lazy { DrawstringView(context) }
 
     init {
+        ensureTarget()
         addView(drawstringView)
     }
 
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+        val width = measuredWidth
+        val height = measuredHeight
+        if (childCount == 0) {
+            return
+        }
+        if (mTarget == null) {
+            ensureTarget()
+        }
+        if (mTarget == null) {
+            return
+        }
+
         drawstringView.layout(
             100,
             100,
             200,
             200
         )
+    }
+
+    private fun ensureTarget() {
+        if (mTarget == null) {
+            for (i in 0 until childCount) {
+                val child = getChildAt(i)
+                if (child != drawstringView) {
+                    mTarget = child
+                    break
+                }
+            }
+        }
     }
 }

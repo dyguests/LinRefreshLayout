4c812a8db4537b770672d87d66d587437eb060f2
fanhl <fanhonglin@klicen.com>
2018-12-03 16:41:56 +0800

    swipe

---
+++ sample/src/main/java/com/fanhl/linrefreshlayout/sample/SwipeActivity.kt
@@ -0,0 +1,12 @@
+package com.fanhl.linrefreshlayout.sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SwipeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_swipe)
+    }
+}

60b2caf37bcebc91ebcf1fe53280c5402cead1ec
fanhl <fanhonglin@klicen.com>
2018-12-03 16:36:38 +0800

    androidx

--- sample/src/main/java/com/fanhl/linrefreshlayout/sample/MainActivity.kt
+++ sample/src/main/java/com/fanhl/linrefreshlayout/sample/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.fanhl.linrefreshlayout.sample
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {

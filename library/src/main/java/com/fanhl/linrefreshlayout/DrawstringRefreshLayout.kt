d8e05125cecdf0f14d1123d541ff6814d07a1ee8
fanhl <fanhonglin@klicen.com>
2018-12-05 14:37:49 +0800

    onInterceptTouchEvent ok

--- library/src/main/java/com/fanhl/linrefreshlayout/DrawstringRefreshLayout.kt
+++ library/src/main/java/com/fanhl/linrefreshlayout/DrawstringRefreshLayout.kt
@@ -2,6 +2,7 @@ package com.fanhl.linrefreshlayout
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
@@ -9,7 +10,9 @@ import android.widget.ListView
 import androidx.annotation.NonNull
 import androidx.annotation.Nullable
 import androidx.core.view.NestedScrollingParent
+import androidx.core.view.ViewCompat
 import androidx.core.widget.ListViewCompat
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 
 /**
  * 拉绳（下拉拉绳）RefreshLayout
@@ -22,17 +25,33 @@ class DrawstringRefreshLayout @JvmOverloads constructor(
     /** the target of the gesture */
     private var mTarget: View? = null
     internal var mRefreshing = false
+    private val mTouchSlop: Int = 0
 
     private var mNestedScrollInProgress: Boolean = false
+
+    internal var mCurrentTargetOffsetTop: Int = 0
+
+    private var mInitialMotionY: Float = 0.toFloat()
+    private var mInitialDownY: Float = 0.toFloat()
+    private var mIsBeingDragged: Boolean = false
+    private var mActivePointerId = INVALID_POINTER
 
     // Target is returning to its start offset because it was cancelled or a
     // refresh was triggered.
     private var mReturningToStart: Boolean = false
 
     /** 下拉view */
-    private val drawstringView by lazy { DrawstringView(context) }
+    private val mCircleView by lazy { DrawstringView(context) }
     /** 下拉view的绘制顺序（之后统一改名） */
     private var mCircleViewIndex = -1
+
+    protected var mOriginalOffsetTop: Int = 0
+
+    internal val mProgress: CircularProgressDrawable by lazy {
+        CircularProgressDrawable(getContext()).apply {
+            setStyle(CircularProgressDrawable.DEFAULT)
+        }
+    }
 
     private var mChildScrollUpCallback: OnChildScrollUpCallback? = null
 
@@ -41,7 +60,7 @@ class DrawstringRefreshLayout @JvmOverloads constructor(
 
         ensureTarget()
 
-        addView(drawstringView)
+        addView(mCircleView)
     }
 
     override fun getChildDrawingOrder(childCount: Int, i: Int): Int {
@@ -79,7 +98,7 @@ class DrawstringRefreshLayout @JvmOverloads constructor(
         val childHeight = height - paddingTop - paddingBottom
         child?.layout(childLeft, childTop, childLeft + childWidth, childTop + childHeight)
 
-        drawstringView.layout(
+        mCircleView.layout(
             0,
             0,
             width,
@@ -104,14 +123,14 @@ class DrawstringRefreshLayout @JvmOverloads constructor(
                 measuredHeight - paddingTop - paddingBottom, View.MeasureSpec.EXACTLY
             )
         )
-//        drawstringView.measure(
+//        mCircleView.measure(
 //            View.MeasureSpec.makeMeasureSpec(100, View.MeasureSpec.EXACTLY),
 //            View.MeasureSpec.makeMeasureSpec(100, View.MeasureSpec.EXACTLY)
 //        )
         mCircleViewIndex = -1
         // Get the index of the circleview.
         for (index in 0 until childCount) {
-            if (getChildAt(index) === drawstringView) {
+            if (getChildAt(index) === mCircleView) {
                 mCircleViewIndex = index
                 break
             }
@@ -133,14 +152,49 @@ class DrawstringRefreshLayout @JvmOverloads constructor(
             return false
         }
 
-        return super.onInterceptTouchEvent(ev)
+        when (action) {
+            MotionEvent.ACTION_DOWN -> {
+                setTargetOffsetTopAndBottom(mOriginalOffsetTop - mCircleView.getTop())
+                mActivePointerId = ev.getPointerId(0)
+                mIsBeingDragged = false
+
+                pointerIndex = ev.findPointerIndex(mActivePointerId)
+                if (pointerIndex < 0) {
+                    return false
+                }
+                mInitialDownY = ev.getY(pointerIndex)
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                if (mActivePointerId == INVALID_POINTER) {
+                    Log.e(LOG_TAG, "Got ACTION_MOVE event but don't have an active pointer id.")
+                    return false
+                }
+
+                pointerIndex = ev.findPointerIndex(mActivePointerId)
+                if (pointerIndex < 0) {
+                    return false
+                }
+                val y = ev.getY(pointerIndex)
+                startDragging(y)
+            }
+
+            MotionEvent.ACTION_POINTER_UP -> onSecondaryPointerUp(ev)
+
+            MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                mIsBeingDragged = false
+                mActivePointerId = INVALID_POINTER
+            }
+        }
+
+        return mIsBeingDragged
     }
 
     private fun ensureTarget() {
         if (mTarget == null) {
             for (i in 0 until childCount) {
                 val child = getChildAt(i)
-                if (child != drawstringView) {
+                if (child != mCircleView) {
                     mTarget = child
                     break
                 }
@@ -162,6 +216,49 @@ class DrawstringRefreshLayout @JvmOverloads constructor(
         } else mTarget!!.canScrollVertically(-1)
     }
 
+    private fun startDragging(y: Float) {
+        val yDiff = y - mInitialDownY
+        if (yDiff > mTouchSlop && !mIsBeingDragged) {
+            mInitialMotionY = mInitialDownY + mTouchSlop
+            mIsBeingDragged = true
+            mProgress.setAlpha(STARTING_PROGRESS_ALPHA)
+        }
+    }
+
+    /**
+     * 这里来控制mCircleView的y轴显示偏移
+     */
+    internal fun setTargetOffsetTopAndBottom(offset: Int) {
+        mCircleView.bringToFront()
+        // FIXME: 2018/12/5 fanhl 之后要优化的地方
+        ViewCompat.offsetTopAndBottom(mCircleView, offset)
+        mCurrentTargetOffsetTop = mCircleView.top
+    }
+
+    private fun onSecondaryPointerUp(ev: MotionEvent) {
+        val pointerIndex = ev.actionIndex
+        val pointerId = ev.getPointerId(pointerIndex)
+        if (pointerId == mActivePointerId) {
+            // This was our active pointer going up. Choose a new
+            // active pointer and adjust accordingly.
+            val newPointerIndex = if (pointerIndex == 0) 1 else 0
+            mActivePointerId = ev.getPointerId(newPointerIndex)
+        }
+    }
+
+    companion object {
+        private const val INVALID_POINTER = -1
+
+        private val LOG_TAG = DrawstringRefreshLayout::class.java.simpleName
+
+        /**
+         * 这里是下拉圈的透明度相关
+         *
+         * FIXME: 2018/12/5 fanhl 之后换成 拉绳自带的逻辑
+         */
+        private val MAX_ALPHA = 255
+        private val STARTING_PROGRESS_ALPHA = (.3f * MAX_ALPHA).toInt()
+    }
 
     /**
      * Classes that wish to be notified when the swipe gesture correctly
